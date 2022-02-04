197de38c8f7cde0f1b4baf93404775037a8d20e6
Maria22 <m.ushakova22@gmail.com>
2022-02-04 14:30:54 +0100

    feat: add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { UserType } from './types/user'
 import { getUsers } from './common/api'
 import { UserList } from './components/UserList/UserList'
